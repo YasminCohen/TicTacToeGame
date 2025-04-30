36fa5b56842f0463172f3d73bf8589f2c0fec882
YasminCohen <yasminco123@gmail.com>
2025-04-30 18:13:58 +0300

    Adding the bonus part

--- main.py
+++ main.py
@@ -36,9 +36,12 @@ def input_name_user():
         except Exception as e:
             print(e)
 
-def player_registration():
+def player_registration(again_computer):
     first_player = input_name_user()
-    second_player = input_name_user()
+    if again_computer:
+        second_player = "Computer"
+    else:
+        second_player = input_name_user()
     sign_array = ["X","O"]
     random.shuffle(sign_array)
     sign_of_first = sign_array[0]
@@ -89,11 +92,18 @@ def input_index_user(player_current, previous_elections):
         except Exception as e:
             print(e)
 
-def put_sign(player_current,player_sign, matrix, previous_elections):
+def put_sign(player_current,player_sign, matrix, previous_elections,again_computer):
     print(f"{player_current}'s turn: ")
     print()
     print_board(matrix, previous_elections)
-    place_on_board = input_index_user(player_current, previous_elections)
+    if again_computer and player_current == "Computer":
+        while True:
+            place_on_board = random_place()
+            if not place_on_board in previous_elections:
+                print(f"The comuter choose the place : {place_on_board} ")
+                break
+    else:
+        place_on_board = input_index_user(player_current, previous_elections)
     name_player = player_current
     sign = player_sign[name_player]
     current_selection = (place_on_board,sign)
@@ -101,6 +111,9 @@ def put_sign(player_current,player_sign, matrix, previous_elections):
     previous_elections[place_on_board] = sign
     update_matrix(place_on_board, matrix, sign)
     if row_check(matrix) or column_check(matrix) or diagonal_check(matrix):
+        print(row_check(matrix))
+        print(column_check(matrix))
+        print(diagonal_check(matrix))
         print(f"The winner is {name_player}!")
         return True
     elif len(previous_elections)== len(matrix)*len(matrix[0]):
@@ -129,22 +142,23 @@ def column_check(matrix):
     return False
 
 def diagonal_check(matrix):
+    flag = True
     for i in range(1,len(matrix)):
-        flag = True
         if matrix[i][i]!= matrix[i-1][i-1]:
             flag = False
             break
     if flag:
         return True
+    flag = True
     for i in range(1, len(matrix)):
-        flag = True
         if matrix[i][len(matrix)-1-i] != matrix[i-1][len(matrix)-i]:
             flag = False
+            break
     if flag:
         return True
     return False
 
-def play_game(player_sign):
+def play_game(player_sign,again_computer):
     print("Welcome to the game, the positions on the board are as follows:")
     matrix = [["0","1","2"],["3","4","5"],["6","7","8"]]
     print_matrix(matrix)
@@ -156,7 +170,7 @@ def play_game(player_sign):
             player_current = list_key[0]
         else:
             player_current = list_key[1]
-        win_or_draw = put_sign(player_current,player_sign,matrix,previous_elections)
+        win_or_draw = put_sign(player_current,player_sign,matrix,previous_elections,again_computer)
         counter+=1
         if win_or_draw:
             return True
@@ -167,11 +181,20 @@ def print_matrix(matrix):
         for j in range(len(matrix[0])):
            print(f"[{matrix[i][j]}]", end=" ")
         print()
+def random_place():
+    number_random = random.randint(0,8)
+    return number_random
 
 def start_game():
-    player_sign = player_registration()
+    playing_again_comuter = input("If you want to play again computer, enter Y, otherwise enter any letter you want.")
+    again_computer = False
+    if playing_again_comuter!="Y":
+        player_sign = player_registration(again_computer)
+    else:
+        again_computer = True
+        player_sign = player_registration(again_computer)
     while True:
-        win_or_draw = play_game(player_sign)
+        win_or_draw = play_game(player_sign,again_computer)
         if win_or_draw:
             restart = input("If you want to play again, enter Y, otherwise enter any letter you want.")
             if restart != "Y":
@@ -179,20 +202,5 @@ def start_game():
     print("game over!")
 if __name__ == '__main__':
     start_game()
-   # result = player_registration()
-   # print(result)
-   # print_board([[0,1,2]
-#                ,[3,4,5],
-#                 [6,7,8]], {0:"O",1: "X",2:"X"},(8,"X"))
-   # player_sign = {"yasmin": "X",
-   #                "tamar": "O"}
-   #
-   # put_sign(player_sign, [[0,1,2],[3,4,5],[6,7,8]], {0:"O",1: "X",2:"X"})
-   # result = row_check([['X','O','X'],['X','O','O'],['O','O','O']])
-   # print(result)
-   # result = column_check([['X','O','O'],['X','O','X'],['X','X','O']])
-   # print(result)
-   # result = diagonal_check([['O', 'O', 'O'],
-   #                          ['X', 'O', 'X'],
-   #                          ['X', 'X', 'O']])
-   # print(result)
+
+
